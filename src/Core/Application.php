2ddabac6a77085a2357ab84befc7aa7aaf5f2a89
git <git@163.com>
2017-02-06 22:30:00 -0500

    Fix bug

--- src/Core/Application.php
+++ src/Core/Application.php
@@ -7,6 +7,7 @@ use Eng\Core\Event\ControllerEvent;
 use Doctrine\Common\Inflector\Inflector;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\Routing\Route;
 
